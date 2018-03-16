be54b2626f330e680c2bcc1a79424225e3f2f24e
genericity <18584146+genericity@users.noreply.github.com>
2018-03-17 11:43:19 +1300

    More minor adjustments and aesthetic tweaks.
    - Made graph 100% width.
    - Moved grade selection box to the bottom of the page.
    - Added link to the title so that clicking on the tool title takes you
    to the first page.
    - Edited columns on student page.
    - Setup basic POST data collection.

--- index.py
+++ index.py
@@ -1,17 +1,36 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import session
+import rpy2.robjects as robjects
+from flask.ext.session import Session
 
+# Define the application.
 app = Flask(__name__)
 app.debug = True
 
+# Start the session.
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
+Session(app)
+
+# R setup.
+r = robjects.r
+
+# App routing.
 @app.route('/')
 def upload_page():
     return render_template('index.html', upload = True)
 
-@app.route('/questions')
+@app.route('/questions', methods = ['GET', 'POST'])
 def questions_page():
-    return render_template('questions.html', questions = True)
+	# Display the questions page.
+	if request.method == 'GET':
+    	return render_template('questions.html', questions = True)
+    # Process the data.
+	if request.method == 'POST':
+		data = request.form
+		return data
 
 @app.route('/grades')
 def grades_page():
@@ -24,3 +43,9 @@ def review_page():
 @app.route('/export')
 def export_page():
     return render_template('export.html', export = True)
+
+def score_students():
+	return
+
+def is_correct_answer():
+	return False
